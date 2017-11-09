6ae4cfee4ef100f2b7f238a38363be8760ee4a12
Mike Prince <mike@tempo.ai>
2017-11-08 16:50:45 -0800

    Added coffee run task

---
+++ tasks/coffee-run/coffee.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.18;
+
+contract LunchOrder 
+{
+    address requestor;
+    uint requestor_stake;
+    uint requestor_terms;
+
+    address runner;
+    uint runner_stake;
+
+    bool delivery_accepted;
+    bool payment_accepted;
+
+    string delivery_rejection_reason;
+    string payment_rejection_reason;
+
+    function LunchOrder(uint stake, string terms) public
+    {
+        requestor = msg.sender;
+        requestor_stake = stake;
+        requestor_terms = terms;
+    }
+
+    function acceptOrder(uint stake) public          
+    {
+        runner = msg.sender;
+        runner_stake = stake
+    }
+    
+    function acceptDelivery() public
+    {
+        if( msg.sender == requestor )
+            delivery_accepted = true;
+    }
+
+    function acceptPayment() public
+    {
+        if( msg.sender == runner )
+            payment_accepted = true;
+    }
+
+    function rejectDelivery( string reason ) public
+    {
+        if( msg.sender == requestor )
+            delivery_rejection_reason = reason;
+    }
+
+    function rejectPayment( string reason ) public
+    {
+        if( msg.sender == runner )
+            payment_rejection_reason = reason;
+    }
+    
+    function kill() public
+    { 
+        if (msg.sender == requestor)  
+            selfdestruct(requestor);
+    }
+}
